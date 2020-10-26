7b6f068696b1b8fceb36e84d7564e35718028241
seed <hellocarloscastillo@gmail.com>
2020-10-26 01:53:06 -0500

    26/oct/2020

--- app/src/main/java/com/example/aquitoyapp/vistas/menuPrincipal.kt
+++ app/src/main/java/com/example/aquitoyapp/vistas/menuPrincipal.kt
@@ -2,7 +2,7 @@ package com.example.aquitoyapp.vistas
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.aquitoyapp.R
 
@@ -12,24 +12,24 @@ class menuPrincipal : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
 
-        btnDosAction()
-        btnTresAction()
-    }
-
-    fun btnTresAction() {
-        findViewById<Button>(R.id.btnTres).setOnClickListener {
+        findViewById<ImageButton>(R.id.btnTres).setOnClickListener {
             val vista = Intent(this, DomiciliosActivosActivity::class.java)
             startActivity(vista)
         }
-    }
-
-    fun btnDosAction() {
-        findViewById<Button>(R.id.btnDos).setOnClickListener {
+        findViewById<ImageButton>(R.id.btnDos).setOnClickListener {
             val vista = Intent(this, DomiciliosDisponiblesActivity::class.java)
             //intent.putExtra("datos_usuario", datos_usuario.toString())
             //finish()
             startActivity(vista)
         }
+    }
+
+    fun btnTresAction() {
+
+    }
+
+    fun btnDosAction() {
+
     }
 
 }
