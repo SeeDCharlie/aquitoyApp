8026fc6c1a1a69e17d2f7e93483f89fc1cfd589e
seed <hellocarloscastillo@gmail.com>
2020-10-31 17:13:07 -0500

    31/oct/2020

---
+++ app/src/main/java/com/example/aquitoyapp/vistas/NuevoClienteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aquitoyapp.vistas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NuevoClienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_cliente)
+    }
+}
