e97f6c1f009768267d0660af83aa542b26cc54d8
seed <hellocarloscastillo@gmail.com>
2020-11-05 17:13:38 -0500

    05/nov/2020

--- app/src/main/java/com/example/aquitoyapp/vistas/GetClienteActivity.kt
+++ app/src/main/java/com/example/aquitoyapp/vistas/GetClienteActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aquitoyapp.vistas
 
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,21 +10,53 @@ import com.example.aquitoyapp.controles.ControlApi
 import com.example.aquitoyapp.modelos.Cliente
 import com.example.aquitoyapp.modelos.rowAdapterClientes
 import org.json.JSONObject
+import java.util.*
+import kotlin.collections.ArrayList
 
 class GetClienteActivity : AppCompatActivity() {
 
     var clientes: ArrayList<Cliente>? = null
+    var clientesAux: ArrayList<Cliente>? = null
     var datosUsuario: JSONObject? = null
     var datosDomicilio: JSONObject? = null
     var controlapi: ControlApi? = null
     var listaClientes: RecyclerView? = null
-
+    var searchv: SearchView? = null
+    var adapter: rowAdapterClientes? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_cliente)
 
         initView()
+
+
+        //eventos
+
+        searchv!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                if (p0!!.isNotEmpty()) {
+                    clientesAux!!.clear()
+                    clientes!!.forEach {
+                        if (it.nombre.toLowerCase(Locale.ROOT)
+                                .contains(p0.toLowerCase(Locale.ROOT))
+                        ) {
+                            clientesAux!!.add(it)
+                        }
+                    }
+                } else {
+                    clientesAux!!.clear()
+                    clientesAux!!.addAll(clientes!!)
+                }
+                listaClientes!!.adapter!!.notifyDataSetChanged()
+                return true
+            }
+
+        })
     }
 
     fun initView() {
@@ -31,10 +64,10 @@ class GetClienteActivity : AppCompatActivity() {
         datosUsuario = JSONObject(intent.getStringExtra("datos_usuario"))
         datosDomicilio = JSONObject(intent.getStringExtra("datos_domicilio"))
         clientes = ArrayList<Cliente>()
+        clientesAux = ArrayList<Cliente>()
+        searchv = findViewById<SearchView>(R.id.schGetClUno)
         listaClientes = findViewById(R.id.listGetClUno)
-        println("antes del de desastre !!!")
         listaClientes?.layoutManager = LinearLayoutManager(this)
-        println("despues del de desastre !!!")
         controlapi!!.getClientes(
             datosUsuario!!.getString("usu_documento"),
             datosUsuario!!.getString("usu_pass"),
@@ -53,10 +86,9 @@ class GetClienteActivity : AppCompatActivity() {
                 )
             )
         }
-        println(">>>>>>>><>>< \n Clientes : " + this.clientes!!.toString())
-
-        val adap = rowAdapterClientes(this.clientes!!)
-        listaClientes?.adapter = adap
+        clientesAux!!.addAll(this.clientes!!)
+        adapter = rowAdapterClientes(this.clientesAux!!)
+        listaClientes?.adapter = adapter
     }
 
 
