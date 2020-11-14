2db44be0e1c4a45fbbb6e40e0369229ea3232d09
seed <hellocarloscastillo@gmail.com>
2020-11-13 20:02:24 -0500

    13/nov/2020

--- app/src/main/java/com/example/aquitoyapp/vistas/DomicilioActivoActivity.kt
+++ app/src/main/java/com/example/aquitoyapp/vistas/DomicilioActivoActivity.kt
@@ -1,22 +1,18 @@
 package com.example.aquitoyapp.vistas
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
 import com.example.aquitoyapp.R
 import com.example.aquitoyapp.controles.ControlApi
 import org.json.JSONObject
-import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
+
 
 class DomicilioActivoActivity : AppCompatActivity() {
 
@@ -24,6 +20,14 @@ class DomicilioActivoActivity : AppCompatActivity() {
     var datosDomicilio: JSONObject? = null
     var switchCamara = -1
     var controlapi: ControlApi? = null
+    var currentPhotoPath: String = ""
+
+    //image pick code
+    private val IMAGE_PICK_CODE = 1000
+
+    //Permission code
+    private val PERMISSION_CODE = 1001
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -116,54 +120,56 @@ class DomicilioActivoActivity : AppCompatActivity() {
 
     //funcion que llama la camara del sistema
     fun getFoto() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            // Ensure that there's a camera activity to handle the intent
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                // Create the File where the photo should go
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    // Error occurred while creating the File
-                    null
-                }
-                // Continue only if the File was successfully created
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        this,
-                        "com.example.android.fileprovider",
-                        it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, 1)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED
+            ) {
+                //permission denied
+                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                //show popup to request runtime permission
+                requestPermissions(permissions, PERMISSION_CODE)
+            } else {
+                //permission already granted
+                pickImageFromGallery()
+            }
+        } else {
+            //system OS is < Marshmallow
+            pickImageFromGallery()
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    //permission from popup granted
+                    pickImageFromGallery()
+                } else {
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
 
-    var currentPhotoPath: String = ""
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "img_${datosDomicilio!!.getInt("dom_id")}_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
-        }
-    }
-
-    // oyente que captura la imagen tomada
     @SuppressLint("MissingSuperCall")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            val imageBitmap = data!!.extras?.get("data") as Bitmap
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             val img = ImageView(this)
-            img.setImageBitmap(imageBitmap)
+            img.setImageURI(data?.data)
             if (switchCamara == 1) {
                 findViewById<LinearLayout>(R.id.lilDoAcUno).addView(img)
             } else if (switchCamara == 0) {
@@ -171,4 +177,6 @@ class DomicilioActivoActivity : AppCompatActivity() {
             }
         }
     }
+
+
 }
