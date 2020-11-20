cddb18e95dc899e5d9c8821cf7f7533257d675cd
seed <hellocarloscastillo@gmail.com>
2020-11-19 22:08:33 -0500

    19/nov/2020 -funcion de reporte de ubicacion implementada

--- app/src/main/java/com/soportec/aquitoyapp/controles/ReporteUbicacion.kt
+++ app/src/main/java/com/soportec/aquitoyapp/controles/ReporteUbicacion.kt
@@ -5,38 +5,77 @@ import android.content.Context
 import android.location.Location
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.soportec.aquitoyapp.modelos.apiInterfaz
+import org.json.JSONObject
 
 
-class ReporteUbicacion(appContext: Context, workerParams: WorkerParameters) :
-    Worker(appContext, workerParams) {
+class ReporteUbicacion(
+    appContext: Context, workerParams: WorkerParameters
+) : Worker(appContext, workerParams), apiInterfaz {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     var lat: Double? = null
     var long: Double? = null
-    var ctx = appContext
+    var control_db = ControlSql(appContext)
 
+    override var context: Context = appContext
+    override var baseUrl: String = "https://soportec.co/mensajeria/webservices/"
+    override var requestExecute: RequestQueue = Volley.newRequestQueue(context)
 
     @SuppressLint("MissingPermission")
     override fun doWork(): Result {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.ctx)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.context)
         while (true) {
 
             fusedLocationClient.lastLocation
                 .addOnSuccessListener { location: Location? ->
                     lat = location?.latitude
                     long = location?.longitude
+
+                    sendLocation(lat!!, long!!)
+
                 }
 
             println("coordenadas de ubicacions: $lat --- $long")
 
-            Thread.sleep(1000 * 5)
+            Thread.sleep(1000 * 10)
         }
 
         return Result.success()
     }
 
+    fun sendLocation(lat: Double, long: Double) {
+        var datos = JSONObject()
+        datos.put("lat", lat)
+        datos.put("long", long)
+        datos.put("actualizar_ubicacion", true)
 
+        var resultado =
+            control_db.getSessions("select * from sesiones where activo = 1 order by id desc;")
+
+        if (!resultado.isEmpty()) {
+            datos.put("usu_id", resultado.get(0).id_user)
+        } else {
+            datos.put("usu_id", null)
+        }
+
+        respuestaPost(datos, "actualizarUbicacion.php")
+    }
+
+    override fun acionPots(obj: JSONObject) {
+        println(obj.getString("msj"))
+    }
+
+    override fun errorOk(obj: JSONObject) {
+        println("error : ${obj.getString("msj")}")
+    }
+
+    override fun errorRequest(msj: String) {
+        println("error : ${msj}")
+    }
 
 }
---
+++ app/src/main/java/com/soportec/aquitoyapp/modelos/UploadInterfaz.kt
@@ -0,0 +1,113 @@
+package com.soportec.aquitoyapp.modelos
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.util.Log
+import android.webkit.MimeTypeMap
+import android.widget.Toast
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.File
+
+interface UploadInterfaz {
+
+
+    var activity: Activity
+    var dialog: ProgressDialog?
+    var serverURL: String
+    var serverUploadDirectoryPath: String
+    val client: OkHttpClient
+    //--------------------------------------------------------------------------------------
+
+
+    fun uploadFile(
+        documento: String,
+        contraseña: String,
+        dom_id: Int,
+        tipo_eviden: Int,
+        sourceFilePath: String,
+        uploadedFileName: String
+    ) {
+        Thread {
+            val sourceFile = File(sourceFilePath)
+            val mimeType = getMimeType(sourceFile)
+            if (mimeType == null) {
+                Log.e("file error", " >>>>>>Not able to get mime type")
+            }
+            val fileName: String =
+                if (uploadedFileName == null) sourceFile.name else uploadedFileName
+            toggleProgressDialog(true)
+            try {
+                val requestBody: RequestBody =
+                    MultipartBody.Builder().setType(MultipartBody.FORM)
+                        .addFormDataPart(
+                            "uploaded_file", fileName,
+                            sourceFile.asRequestBody(mimeType?.toMediaTypeOrNull())
+                        ).addFormDataPart(
+                            "documento", documento
+                        ).addFormDataPart(
+                            "contraseña", contraseña
+                        ).addFormDataPart(
+                            "guardar_evidencia", true.toString()
+                        ).addFormDataPart(
+                            "dom_id", dom_id.toString()
+                        ).addFormDataPart(
+                            "tipo_eviden", tipo_eviden.toString()
+                        ).build()
+
+                val request: okhttp3.Request =
+                    okhttp3.Request.Builder().url(serverURL).post(requestBody).build()
+
+                val response: Response = client.newCall(request).execute()
+
+                if (response.isSuccessful) {
+                    Log.d("File upload", "success, path: $serverUploadDirectoryPath$fileName")
+                    val jResponse = JSONObject(response.body!!.string()).getJSONObject("dats")
+                    showToast(jResponse.getString("msj"))
+                } else {
+                    Log.e("File upload", "fallo al cargar la imagen")
+                    showToast("Fallo al cargar la imagen!")
+                }
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                Log.e("File upload", ex.toString())
+                showToast("File uploading failed : " + ex)
+            }
+            toggleProgressDialog(false)
+        }.start()
+    }
+
+    // url = file path or whatever suitable URL you want.
+    fun getMimeType(file: File): String? {
+        var type: String? = null
+        val extension = MimeTypeMap.getFileExtensionFromUrl(file.path)
+        if (extension != null) {
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+        }
+        return type
+    }
+
+    fun showToast(message: String) {
+        activity.runOnUiThread {
+            Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun toggleProgressDialog(show: Boolean) {
+        activity.runOnUiThread {
+            if (show) {
+                @Suppress("DEPRECATION")
+                dialog = ProgressDialog.show(activity, "Aquitoy Msj", "Cargando Imagen...", true)
+            } else {
+                dialog?.dismiss()
+            }
+        }
+    }
+
+
+}
